923c486997b694079830fa545b854e59dd4d2ee1
Mohammad Alnabulsieh <mohammadalnabulsih@gmail.com>
2025-11-26 15:15:04 +0300

    mo

---
+++ taskThree.js
@@ -0,0 +1,11 @@
+/*function tellFortune(job, location, partner , children){
+  let tellFortune = "You will be a "+ job + " in " + location +", and married to "+ partner +" with " +children+ " kids.";
+  return tellFortune ;
+}
+console.log(tellFortune("web dev", "Amman", "kammar" , 4));*/
+//-------------------------------------------------------------------------
+function calculateDogAge(dogAge){
+  newDogAge = dogAge * 7;
+  return "Your doggie is "+ newDogAge +" years old in dog years!";
+}
+console.log(calculateDogAge(2));
